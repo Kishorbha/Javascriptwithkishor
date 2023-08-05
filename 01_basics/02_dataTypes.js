ce45dceb0223ebd4685990ad547b6498eac0fffa
kishor Bhandari <86872670+Kishorbha@users.noreply.github.com>
2023-08-05 17:32:07 +0000

    feat(Basic) : data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+//There are 2 types of data types in JS
+
+// Primitive : call by its vlaue.
+
+// 7 types : String, Number, Boolean, undefined, null, Symbol, BigInt 
+
+//Non-primitive/Refrence : call by its refrence.
+
+//Array, Objects, Functions
