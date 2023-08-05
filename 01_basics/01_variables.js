e194d438fc8820b122cd2221005f1e72f980e9fa
kishor Bhandari <86872670+Kishorbha@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    feat(Basics) : variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=553344 /** Variable constant */
+let email='kishor@gmail.com' /** Variable let */
+var password="kishor@123" /** Variable var not preferable to use beacause of issue on functional and block scope */
+city="Bhimdatta"
+
+
+/**varible which is declared with const can not re assign any value  */
+// accountId=123421 
+ 
+email="kishor123@gmail.com"
+password="kishor"
+
+console.table([accountId,email,password])
